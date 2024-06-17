33158b93f6c55d76b380de82f4c18a1afade9d7e
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-06-17 19:57:51 -0400

    Removed webpack.

--- README.md
+++ README.md
@@ -3,12 +3,3 @@
 Demonstrates how to play encrypted video in the remote player.
 
 See the [Protected Content](https://developer.synamedia.com/senza/docs/protected-content-1) tutorial in the Senza developer documentation.
-
-## Build
-
-```bash
-npm install
-npm ci
-npx webpack --config webpack.config.js
-open index.html
-```
--- tears.js
+++ tears.js
@@ -1,39 +1,37 @@
-import { init, remotePlayer, uiReady, lifecycle } from "@Synamedia/hs-sdk";
-
 const ENCRYPTED_TEST_VIDEO = "https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd";
+const WIDEVINE = "https://proxy.uat.widevine.com/proxy";
 
 window.addEventListener("load", async () => {
-  await init();
-  uiReady();
+  await hs.init();
+  hs.uiReady();
 });
 
 document.addEventListener("keydown", async (event) => {
-  const currentState = await lifecycle.getState();
+  const currentState = await hs.lifecycle.getState();
   if (currentState === "background" || currentState === "inTransitionToBackground") {
-    lifecycle.moveToForeground();
+    hs.lifecycle.moveToForeground();
   } else {
-    await remotePlayer.load(ENCRYPTED_TEST_VIDEO);
-    remotePlayer.play();
+    await hs.remotePlayer.load(ENCRYPTED_TEST_VIDEO);
+    hs.remotePlayer.play();
   }
 }, false);
 
-remotePlayer.addEventListener("license-request", async (event) => {
-  console.log("Got license-request event");
+hs.remotePlayer.addEventListener("license-request", async (event) => {
+  console.log("[tears]", "Got license-request event");
   const requestBuffer = event?.detail?.licenseRequest;
   const requestBufferStr = String.fromCharCode.apply(null, new Uint8Array(requestBuffer));
-  console.log("License request in base64:", requestBufferStr);
-
   const decodedLicenseRequest = window.atob(requestBufferStr); // from base 64
+  console.log("[tears]", "License request in base64:", decodedLicenseRequest);
   const licenseRequestBytes = Uint8Array.from(decodedLicenseRequest, (l) => l.charCodeAt(0));
 
   const res = await getLicenseFromServer(licenseRequestBytes.buffer);
-  console.log("Writing response to platform ", res.code, res.responseBody);
+  console.log("[tears]", "Writing response to platform ", res.code, res.responseBody);
   event.writeLicenseResponse(res.code, res.responseBody);
 });
 
 async function getLicenseFromServer(licenseRequest) {
-  console.log("Requesting license From Widevine server");
-  const response = await fetch("https://proxy.uat.widevine.com/proxy", {
+  console.log("[tears]", "Requesting license From Widevine server");
+  const response = await fetch(WIDEVINE, {
     "method": "POST",
     "body": licenseRequest,
     "headers" : {
@@ -43,9 +41,9 @@ async function getLicenseFromServer(licenseRequest) {
 
   const code = response.status;
   if (code !== 200) {
-    console.error("failed to to get response from widevine:", code);
+    console.error("[tears]", "failed to to get response from widevine:", code);
     const responseBody = await response.text();
-    console.error(responseBody);
+    console.error("[tears]", responseBody);
     return {code, responseBody};
   }
   const responseBody = await response.arrayBuffer();
