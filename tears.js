ce61a47058117f21bb3ff3eaa4b897f2b0724ceb
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2025-03-05 11:34:03 -0500

    Switched to a different stream. Added error handling.

--- tears.js
+++ tears.js
@@ -1,8 +1,19 @@
-const ENCRYPTED_TEST_VIDEO = "https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd";
-const WIDEVINE = "https://proxy.uat.widevine.com/proxy";
+const ENCRYPTED_TEST_VIDEO = "https://cdn.bitmovin.com/content/assets/art-of-motion_drm/mpds/11331.mpd";
+const WIDEVINE = "https://cwip-shaka-proxy.appspot.com/no_auth";
+
+// const ENCRYPTED_TEST_VIDEO = "https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd";
+// const WIDEVINE = "https://proxy.uat.widevine.com/proxy";
 
 window.addEventListener("load", async () => {
   await senza.init();
+
+  senza.remotePlayer.addEventListener("error", (event) => {
+    console.log("[tears]", "remotePlayer error:", event.detail.errorCode, event.detail.message);
+    if (event.detail.errorCode == 3401) {
+      console.log("[tears]", "Content from this server is not compatible with Senza. Check that the server supports the TLS1.2 cipher suite.");
+    }
+  });
+
   senza.uiReady();
 });
 
@@ -11,12 +22,15 @@ document.addEventListener("keydown", async (event) => {
   if (currentState === "background" || currentState === "inTransitionToBackground") {
     senza.lifecycle.moveToForeground();
   } else {
+    console.log("[tears]", "Loading", ENCRYPTED_TEST_VIDEO);
     await senza.remotePlayer.load(ENCRYPTED_TEST_VIDEO);
-    senza.remotePlayer.play();
+    console.log("[tears]", "Playing");
+    await senza.remotePlayer.play();
+    await senza.lifecycle.moveToBackground();
   }
 }, false);
 
-hs.remotePlayer.addEventListener("license-request", async (event) => {
+senza.remotePlayer.addEventListener("license-request", async (event) => {
   console.log("[tears]", "Got license-request event");
   const requestBuffer = event?.detail?.licenseRequest;
   const requestBufferStr = String.fromCharCode.apply(null, new Uint8Array(requestBuffer));
@@ -49,4 +63,3 @@ async function getLicenseFromServer(licenseRequest) {
   const responseBody = await response.arrayBuffer();
   return {code, responseBody};
 }
-
